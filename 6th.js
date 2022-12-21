d686a8ff444e78e316c421b1449c4959c7291143
mahmoodmuzammil88 <muzammil9676@gmail.com>
2022-12-21 23:44:11 +0530

    ..

--- 6th.js
+++ 6th.js
@@ -1,19 +1,5 @@
 //Q.Find the largest element in an array.
 
-function max(arr){
-    let max = arr[0];
-
-    for(let i=0; i<=arr.length; i++){
-        if(arr[i]>max){
-            max = arr[i]
-        }
-    }console.log(max)
-}max(1,2,3,4)
-
-
-
-
-
 
 
 
--- 8th.js
+++ 8th.js
@@ -1,5 +1,2 @@
 // Find the given element in an array and return it's index if present else -1;
 
-function hey(){
-
-}hey()
