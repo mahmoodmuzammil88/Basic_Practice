c7ab7090c4b5b975761eb6f56f6c49190e73d64a
mahmoodmuzammil88 <muzammil9676@gmail.com>
2023-01-05 22:53:18 +0530

    new

--- Day7/BS.js
+++ Day7/BS.js
@@ -11,3 +11,27 @@
 //         else r = mid-1
 //     }return -1;
 // };search([-1,0,3,5,9,12],9)
+
+// function findFirstOccu(arr, target) {
+//     let start = 0;
+//     let last = arr.length - 1;
+//     let ans = 0;
+  
+//     while (start <= last) {
+//       let mid = Math.floor((last + start) / 2);
+//       if (arr[mid] === target) {
+//         ans = mid;
+//         last = mid - 1;
+//       } else {
+//         start = mid + 1;
+//       }
+//     }
+//     console.log(ans);
+//   }
+
+function changeBit(num, n) {
+    let a = 1 << n;
+    console.log(a);
+    let res = num | a;
+    console.log(res);
+  }
---
+++ Day8/index.js
@@ -0,0 +1,17 @@
+
+// Count number of occurrences of an element
+    // function countOccurrences(arr,n,x)
+    // {
+    //     let res = 0;
+    //     for (let i=0; i<n; i++)
+    //     {
+    //         if (x == arr[i])
+    //             res++;
+    //     }
+    //     return res;
+    // }
+    // arr=[1, 2, 2, 2, 2, 3, 4, 7 ,8 , 2, 8]
+    // let  n = arr.length;
+    // let x = 8;
+    // console.log(countOccurrences(arr, n, x));
+// ----------------------------------------------------------------------------------
