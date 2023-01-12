33ce29a0b4528cb9c135cf0484272a56d661af79
mahmoodmuzammil88 <muzammil9676@gmail.com>
2023-01-12 13:17:13 +0530

    new

--- Day11/index.js
+++ Day11/index.js
@@ -3,11 +3,11 @@
 //  An object is an instance of a class which has those properties and behaviours attached.
 
 
-class student{
-    constructor(name, roolno, section, standard){
-        this.name = name;
-        this.rool_no = rool_no;
-        this.section = section;
-        this.standard = standard
-    }
-}
+// class student{
+//     constructor(name, roolno, section, standard){
+//         this.name = name;
+//         this.rool_no = rool_no;
+//         this.section = section;
+//         this.standard = standard
+//     }
+// }
